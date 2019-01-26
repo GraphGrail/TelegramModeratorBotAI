992ab9f6f5228d1a164216fbb7e2dd24d4819251
dmlitov4 <dmitriyonline222@gmail.com>
2019-01-26 14:39:20 +0300

    Updating project with file that shows results for input text

---
+++ testing_model.py
@@ -0,0 +1,14 @@
+import os
+from commentEvaluator import CommentEvaluator
+from keras.models import load_model
+import pickle
+
+script_dir = os.path.dirname(__file__)
+with open(os.path.join(script_dir, 'model_files/token.pickle'), 'rb') as handle:
+    tok = pickle.load(handle)
+
+model = load_model(os.path.join(script_dir, 'model_files/rus_weights2.hdf5'))
+evaluator = CommentEvaluator(model, tok)
+text = input('Введите текст для проверки: ')
+print(evaluator.analyze(text))
+print()
